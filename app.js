74ff98b9701974efd296a3010f4d372fce0858da
robelperey <robelperey@gmail.com>
2023-02-25 07:33:04 +0800

    add basic math functions

---
+++ app.js
@@ -0,0 +1,35 @@
+// grab elements from html
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+    case "=":
+      return operate(operator, x, y);
+
+    default:
+      break;
+  }
+}
